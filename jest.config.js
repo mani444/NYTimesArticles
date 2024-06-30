e2b8fc7116fa2d65ed9515c8721c2438b873c39a
Abdul Mannan <abdulmannan3111@gmail.com>
2024-06-30 16:21:49 +0500

    add unit tests

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+
+  testEnvironment: "jsdom",
+
+   transform: {
+
+     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+
+   },
+
+   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+ };
